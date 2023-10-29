23c09ebaa10d5d366a5bcb1d4881a9b3f6eff70f
Tarun Lalwani <tarunlalwani2002@gmail.com>
2023-10-29 13:47:04 +0530

    Hero section, Navbar

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import  Footer  from '@/components/Footer';
+import  Navbar  from '@/components/Navbar';
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+}
